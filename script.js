c0f4b94c9f6f35e76b65b7216b18f4456d401d72
Hilder Arrieta <38758814+matlab98@users.noreply.github.com>
2021-07-10 23:01:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,16 @@ const ball = document.getElementById("ball");
     // Obtenemos el modo actual.
     if (localStorage.getItem('dark-mode') === 'true') {
       document.body.classList.add('dark');
-      ground.classList.add('night');
-      ball.className = 'moon ball';
     } else {
       document.body.classList.remove('dark');
-      ground.classList.remove('night');
-      ball.className = 'ball';
     }
 
 chk.addEventListener("change", () => {
 
     document.body.classList.toggle('dark');
     if (ground.className == 'icon') {
-      ground.classList.add('night');
-      ball.className = 'moon ball';
       localStorage.setItem('dark-mode', 'true');
     } else {
-      ground.classList.remove('night');
-      ball.className = 'ball';
       localStorage.setItem('dark-mode', 'false');
     }
 });
