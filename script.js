c0c4b73f6a414517cf88de17f8be388345030f10
Hilder Arrieta <38758814+matlab98@users.noreply.github.com>
2021-07-10 22:28:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,17 @@ const ball = document.getElementById("ball");
 const contBall = document.getElementById("container-icon");
 
 chk.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
+  const ground = document.getElementById('back');
+    const ball = document.getElementById('ball');
 
-  if (ground.getAttribute("src") == "light.jpeg") {
-    ground.src = "night.jpeg";
-    ground.className = "icon-dark";
-	contBall.className="ball moon-shadow";
-    ball.src = "moon.svg";
-	ball.className = "moon";
-  } else {
-    ground.src = "light.jpeg";
-    ground.className = "icon-light";
-	contBall.className="ball sun-shadow";
-    ball.src = "https://img.icons8.com/128/fluent/000000/weather.png";
-
-
-    ball.className = "sun";
-  }
+    document.body.classList.toggle('dark');
+    if (ground.className == 'icon') {
+      ground.classList.add('night');
+      ball.className = 'moon ball';
+      localStorage.setItem('dark-mode', 'true');
+    } else {
+      ground.classList.remove('night');
+      ball.className = 'ball';
+      localStorage.setItem('dark-mode', 'false');
+    }
 });
