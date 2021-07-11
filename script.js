a3ac2fa27083c3da9e53ef739e367345ed010656
Hilder Arrieta <38758814+matlab98@users.noreply.github.com>
2021-07-10 22:43:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const chk = document.getElementById("chk");
 const ground = document.getElementById("back");
 const ball = document.getElementById("ball");
 
+const ground = document.getElementById('back');
+    const ball = document.getElementById('ball');
+    // Obtenemos el modo actual.
+    if (localStorage.getItem('dark-mode') === 'true') {
+      document.body.classList.add('dark');
+      ground.classList.add('night');
+      ball.className = 'moon ball';
+    } else {
+      document.body.classList.remove('dark');
+      ground.classList.remove('night');
+      ball.className = 'ball';
+    }
+
 chk.addEventListener("change", () => {
 
     document.body.classList.toggle('dark');
